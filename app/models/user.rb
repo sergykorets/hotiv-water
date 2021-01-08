449dadab908da865c204f3d15bcfbea4cefc8a72
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2021-01-08 18:26:18 +0200

    client

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  enum role: [:cashier, :admin]
+  enum role: [:client, :admin]
 
   has_many :reservations
 
